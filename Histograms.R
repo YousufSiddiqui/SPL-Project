2082d7998459c323403199e02c4cbc00b4ed8baf
YousufSiddiqui <siddiquy@student.hu-berlin.de>
2016-06-08 16:25:49 +0200

    cleaned up code for histogram

--- Histograms.R
+++ Histograms.R
@@ -1,68 +1,55 @@
 #####Histograms######
+graphics.off()
 
 #Variable: Months
 
-max(C_D$Months)
-min(C_D$Months)
+par(mfrow=c(3,4))
 
 h1 = hist(C_D$Months,col = "blue",breaks = 10,ylim = c(0,300),
-          xlim = c(0,80),xlab = "No.of Months", 
+          xlim = c(min(C_D$Months),max(C_D$Months)),xlab = "No.of Months", 
           main = "Histogram of Variable: Months")
 
 #Variable:Age
 
-max(C_D$Age)
-min(C_D$Age)
+
+
 
 h2 = hist(C_D$Age,col = "blue",breaks = 20,ylim = c(0,150),
-          xlim = c(10,80),xlab = "Age in Years", 
+          xlim = c(min(C_D$Age),max(C_D$Age)),xlab = "Age in Years", 
           main = "Histogram of Variable: Age")
 
 #Variable:Credit Amount
 
-max(C_D$Credit_Amount)
-min(C_D$Credit_Amount)
 
 h3 = hist(C_D$Credit_Amount,col = "blue",breaks = 50,ylim = c(0,220),
-          xlim = c(200,20000),xlab = "Credit Amount", 
+          xlim = c(min(C_D$Credit_Amount),max(C_D$Credit_Amount)),xlab = "Credit Amount", 
           main = "Histogram of Variable: Credit_Amount")
 
 #Variable: Residence Since 
 
-max(C_D$Residence_Since)
-min(C_D$Residence_Since)
-
 h4 = hist(C_D$Residence_Since,col = "blue",breaks = 5,ylim = c(0,220),
-          xlim = c(0,5),xlab = "Residence_Since", 
+          xlim = c(min(C_D$Residence_Since),max(C_D$Residence_Since)),xlab = "Residence_Since", 
           main = "Histogram of Variable: Residence_Since")
 
-lines(density(C_D$Residence_Since, adjust = 220), col = "orange")
+#lines(density(C_D$Residence_Since, adjust = 220), col = "orange")
 
 #Variable: Installment Rate
 
-max(C_D$Installment_Rate)
-min(C_D$Installment_Rate)
-
 h5 = hist(C_D$Installment_Rate,col = "blue",breaks = 5,ylim = c(0,600),
-          xlim = c(0,4),xlab = "Installment Rate", 
+          xlim = c(min(C_D$Installment_Rate),max(C_D$Installment_Rate)),xlab = "Installment Rate", 
           main = "Histogram of Variable: Installment Rate")
 
 #Variable: Existing Credit
 
-max(C_D$Existing_Credit)
-min(C_D$Existing_Credit)
 
 h6 = hist(C_D$Existing_Credit,col = "blue",breaks = 5,ylim = c(0,700),
-          xlim = c(0,5),xlab = "Existing Credit", 
+          xlim = c(min(C_D$Existing_Credit),max(C_D$Existing_Credit)),xlab = "Existing Credit", 
           main = "Histogram of Variable: Existing Credit")
 
 
 #Variable: Dependents
 
-max(C_D$Dependents)
-min(C_D$Dependents)
-
 h7 = hist(C_D$Dependents,col = "blue",breaks = 4,ylim = c(0,1000),
-          xlim = c(0,3),xlab = "Dependents", 
+          xlim = c(min(C_D$Dependents),max(C_D$Dependents)),xlab = "Dependents", 
           main = "Histogram of Variable: Dependents")
 
